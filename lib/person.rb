d74e7058baed09c5b4c954a00f9968d22725b9d2
Ethan Collins <ethantcollins98@gmail.com>
2019-07-16 17:05:29 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+    def initialize 
+        @name
+        @job
+    end
+
+    def name 
+        @name
+    end
+
+    def name= newName
+        @name = newName
+    end
+
+    def job
+        @job
+    end
+
+    def job= newJob
+        @job = newJob
+    end
+end
